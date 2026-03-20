7d6b7bd1a04d072772b16c7991cfff479c84c127
prashantrajj1 <prashant75490@gmail.com>
2026-03-20 10:04:07 +0530

    update features

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
     <footer>
-        <p>&copy; <?php echo date('Y'); ?> Train Management System. Designed for DBMS Project.</p>
+        <p>&copy; <?php echo date('Y'); ?> Developed by Prashant Kumar & Manish Ghatuary.</p>
     </footer>
     <script src="/tms/assets/js/main.js"></script>
 </body>
